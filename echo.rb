d429ce37a6a018a1c80392ee95aa4b24820c96bb
guppy0356 <a_noguchi@libertyfish.co.jp>
2018-01-24 06:57:22 +0900

    Add echo server script

---
+++ echo.rb
@@ -0,0 +1,13 @@
+require "socket"
+
+port = 8000
+server = TCPServer.open(port)
+
+loop do
+  Thread.start(server.accept) do |buffer|
+    while buffer.gets
+      buffer.write($_)
+    end
+    buffer.close
+  end
+end
